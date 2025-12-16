27a06f8711b79c96ea7933aca096f2d697bd49f6
Tofunmi reah <oluwasolapeilesanmi@gmail.com>
2025-12-16 08:16:10 +0100

    scaled dice animation on rolling

--- craps-game/crapsgame.js
+++ craps-game/crapsgame.js
@@ -18,6 +18,8 @@ const crapsStatsUsername = "craps-stats-username"
 const crapsStatsMoney = "craps-stats-money"
 const crapsStatsRounds = "craps-stats-rounds"
 const crapsUserBetAmount = "craps-user-bet-amount"
+const crapsRollDiceButton = "craps-roll-dice-button"
+const crapsRollDiceAnimationContainer = "craps-roll-dice-animation-container"
 
 // In-game variables
 let currentRounds = startingRounds
@@ -94,8 +96,20 @@ function setBetAmount (betAmount) {
 }
 
 function rollDice () {
-    const diceRollElement = document.getElementById("craps-roll-dice-animation-container")
-    rollADie({ element: diceRollElement, numberOfDice: 2, callback: processDiceResult });
+    formatDiceScale()
+    document.getElementById(crapsRollDiceButton).style.display = "none"
+    const diceRollElement = document.getElementById(crapsRollDiceAnimationContainer)
+    rollADie({ element: diceRollElement, numberOfDice: 2, callback: processDiceResult, delay: 10000000});
+}
+
+function formatDiceScale () {
+    const vw = window.innerWidth * 80
+    const vh = window.innerHeight * 0.8
+    const widthScale = Math.min(700, vw, vh)
+    const heightScale = widthScale * 0.714
+    const scale = heightScale / 391.272
+    alert(scale)
+    document.getElementById(crapsRollDiceAnimationContainer).style.transform = "scale(" + scale + ")"
 }
 
 function processDiceResult (diceResult) {
